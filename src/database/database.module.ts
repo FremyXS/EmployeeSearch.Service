60039d8ca2b2eb1ce0584692c63444d282f1047a
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-08 16:01:39 +0500

    refactor(database): change module - add database config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataBaseSource } from './services/database.config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DataBaseSource,
+    }),
+  ],
+})
 export class DatabaseModule {}
